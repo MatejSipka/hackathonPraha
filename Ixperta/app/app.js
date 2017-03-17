8d18c415e8189d38642cae964b03b682f1122f5c
Šilhan Jan <jan.silhan@ixperta.com>
2017-03-18 00:49:13 +0100

    Migrate to root

--- Ixperta/app/app.js
+++
@@ -1,9 +0,0 @@
-var applicationModule = require("application");
-
-if(applicationModule.ios) {
-  GMSServices.provideAPIKey("AIzaSyDiuRwKIU044ppK_GHVCZxcDHpw3aVpYh4");
-}
-
-
-
-applicationModule.start({ moduleName: "main-page" });
