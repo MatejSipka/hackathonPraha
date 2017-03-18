2601ec1d4ac899487fe2c87ce4ff1c37bc99b0c9
matej.sipka <matej.sipka@ixperta.com>
2017-03-18 03:54:34 +0100

    position wather

--- app/main-page.js
+++ app/main-page.js
@@ -1,6 +1,7 @@
 var mapsModule = require("nativescript-google-maps-sdk");
 var totalyOPModule = require('./actions/fab').test;
 var tNSTextToSpeech = require("nativescript-texttospeech");
+var mojio = require("./actions/mojio");
 
 function onMapReady(args) {
     var mapView = args.object;
@@ -19,7 +20,7 @@ function onMapReady(args) {
 let TTS = new tNSTextToSpeech.TNSTextToSpeech();
 
 let speakOptions = {
-    text: 'Whatever you like', /// *** required ***
+    text: 'Will the real Slim Shady please stand up... I repeat, will the real Slim Shady please stand up', /// *** required ***
     speakRate: 1.0, // optional - default is 1.0
     pitch: 1.0, // optional - default is 1.0
     volume: 5.0, // optional - default is 10
@@ -28,7 +29,7 @@ let speakOptions = {
 }
 
 // Call the `speak` method passing the SpeakOptions object
-TTS.speak(speakOptions);
+// TTS.speak(speakOptions);
 
 function onMarkerSelect(args) {
 
